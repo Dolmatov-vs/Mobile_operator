a78497746dfc949b0dad92f119eae411a521eff0
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-05-01 13:28:58 +0300

    Edit project

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int AccountInKopecks = 10000;
-        int AdmissionInKopecks = 110000;
-        if (AdmissionInKopecks < 100000){
-            AccountInKopecks = (AccountInKopecks + AdmissionInKopecks) / 100;
-            System.out.println(AccountInKopecks);
+        int Account = 10000; // В копейках
+        int Admission = 110000; // В копейках
+        if (Admission < 100000){
+            Account = (Account + Admission) / 100;
+            System.out.println(Account);
         }
-        int Bonus = AdmissionInKopecks / 10000;
-        AccountInKopecks = Bonus + ((AccountInKopecks + AdmissionInKopecks) / 100);
-        System.out.println(AccountInKopecks);
+        int Bonus = Admission / 10000;
+        Account = Bonus + ((Account + Admission) / 100);
+        System.out.println(Account);
     }
 }
