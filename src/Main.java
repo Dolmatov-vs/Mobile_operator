3d2c96078fc2e3252fd6aee449ff072f5fb0b498
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-05-01 14:27:49 +0300

    Edit else

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int account = 10000; // В копейках
-        int admission = 110000; // В копейках
-        if (admission < 100000){
-            account = (account + admission) / 100;
-            System.out.println(account);
-        }
-        else {
+        int admission = 10000; // В копейках
+        if (admission >= 100000){
             int bonus = admission / 10000;
             account = bonus + ((account + admission) / 100);
+            System.out.println(account);
+        }   else {
+            account = (account + admission) / 100;
             System.out.println(account);
         }
     }
